5a43d4598ceb294f85b280f974663af6c1da2139
weixinfei <weixinfei@appchina.com>
2018-04-07 01:21:05 +0800

    升级部分依赖版本，添加OkHttp缓存(1)

--- app/src/main/java/com/zurich/gankmaterial/retrofit/http/OKHttpFactory.java
+++ app/src/main/java/com/zurich/gankmaterial/retrofit/http/OKHttpFactory.java
@@ -2,10 +2,13 @@ package com.zurich.gankmaterial.retrofit.http;
 
 
 import com.zurich.gankmaterial.BuildConfig;
+import com.zurich.gankmaterial.GankApplication;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Cache;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
@@ -48,6 +51,15 @@ public enum OKHttpFactory {
         if (BuildConfig.DEBUG) {
             builder.addInterceptor(httpLoggingInterceptor);
         }
+
+        // 设置缓存目录
+        File cacheDir = new File(GankApplication.getContext().getCacheDir(), "response");
+        Cache cache = new Cache(cacheDir, 10 * 1024 * 1024);
+        builder.cache(cache);
+
+        // 添加缓存拦截器
+        builder.addInterceptor(new CacheInterceptor());
+
         okHttpClient = builder.build();
 
     }
